f1984387dad2eac1c43c45db29ceeeb9649d88d0
wulandarikaranamu <wulandarikaranamu02@gmail.com>
2021-12-21 05:09:22 -0800

    komentar

---
+++ listdantuple.py
@@ -0,0 +1,38 @@
+print('=' * 20, "LIST", '=' * 20)
+
+#no 1
+warna = ['merah', 'kuning', 'hijau', 'biru', 'ungu']
+print(warna)
+#no 2
+banyak_warna = len(warna)
+print(banyak_warna)
+#no 3
+warna[1] = 'jingga'
+print(warna) 
+#no 4
+print(warna[0])
+#no 5
+print(warna[-1])
+#no 6
+print(warna[2 : 4])
+#no 7
+warna.append('putih')
+print(warna)
+#no 8
+warna.extend(['pink', 'hitam'])
+print(warna)
+#no 9
+warna.insert(3, 'coklat')
+print(warna)
+#no 10
+del warna[1]
+print(warna)
+#no 11
+warna.remove('biru')
+print(warna)
+#no 12
+warna.pop
+print(warna)
+#no 13
+for wrn in warna :
+    print(wrn)
